d8c6eef6f2348709372c27af0e0606459403ed5d
Harry Zoeftig <harry.zoeftig@btinternet.com>
2025-10-28 15:18:58 +0000

    added code outline & setup world file

--- controllers/drone_controller/drone_controller.py
+++ controllers/drone_controller/drone_controller.py
@@ -1,14 +1,23 @@
+from mapping import Mapping
 """drone_controller controller."""
 
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
 from controller import Robot
+from controller import Keyboard
 
 # create the Robot instance.
 robot = Robot()
 
 # get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
+
+# create keyboard instance
+keyboard=Keyboard()
+keyboard.enable(timestep)
+
+# create the map
+mapping = Mapping()
 
 # You should insert a getDevice-like function in order to get the
 # instance of a device of the robot. Something like:
@@ -19,14 +28,15 @@ timestep = int(robot.getBasicTimeStep())
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
 while robot.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
-
-    # Process sensor data here.
-
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
+    key=keyboard.getKey()
+    if (key == "w"):
+        pass
+        #go forward
+    #...
+    elif (key == "r"):
+        # Ben
+        
+    #localisation -> mapping -> databse of map
     pass
 
 # Enter here exit cleanup code.
---
+++ controllers/drone_controller/mapping.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+class Mapping:
+
+    map = []
+    start_cell = (0,0)
+    drone_size = (0,0) # update from docs
+
+    def __init__(self):
+        pass
+        
+    def getMap(self):
+        return np.array(map)
