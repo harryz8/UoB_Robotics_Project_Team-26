4cc4f689010cfdf9d843359f4541f48e8836db19
Harry Zoeftig <harry.zoeftig@btinternet.com>
2025-11-03 10:55:53 +0000

    one idea of how to update probs

--- controllers/drone_controller/mapping.py
+++ controllers/drone_controller/mapping.py
@@ -14,10 +14,16 @@ class Mapping:
         self.field_of_view = lidar.getFov()
         self.max_range = lidar.getMaxRange()
         
-    def update(self):
+    def _valid_map_index(index: Tuple[int,int,int]):
+        index_arr = np.array(index)
+        return (index_arr >= 0).all() and (index_arr < self.map.shape).all()
+        
+    def update(self, ism, prior, loc):
+        current_cell = (loc[0]/self.block_length, loc[1]/self.block_length, loc[2]/self.block_length)
         range_image_vec = np.array(self.lidar.getRangeImage())
         block_depths = range_image_vec // (self.block_length/1000)
-        
+        self.map = np.log(ism / (1-ism)) + np.log((1-prior)/prior) + self.map
+        return np.array(self.map)
         
     def get(self):
         return np.array(map)
@@ -26,4 +32,6 @@ class Mapping:
         """
         Returns occupancy for specified co-ordinate
         """
+        if not self._valid_map_index((x,y,z)):
+            raise IndexError("Index out of range for this map")
         return map[x,y,z]
