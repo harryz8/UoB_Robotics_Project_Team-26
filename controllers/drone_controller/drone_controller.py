c3d5eb5f855618e53dac470367a292494b20e3f9
Harry Zoeftig <harry.zoeftig@btinternet.com>
2025-11-07 19:59:40 +0000

    changed mapping import, renamed mapping instance, suggested how mapping and path_planner interact

--- controllers/drone_controller/drone_controller.py
+++ controllers/drone_controller/drone_controller.py
@@ -1,16 +1,15 @@
 """drone_controller controller."""
-from mapping import Mapping
+from mapping import *
 from path_planner import Path_Planner
 import numpy as np
-import math
 
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
 from controller import Robot
 from controller import Keyboard
 
-BLOCK_LENGTH: float = 350  #mm
-ROBOT_SIZE: int = 1  #block
+BLOCK_LENGTH: float = 350  # mm
+ROBOT_SIZE: int = 1  # block
 
 # create the Robot instance.
 robot = Robot()
@@ -44,7 +43,7 @@ lidar.enable(timestep)
 # lidar.enablePointCloud()
 
 # create the map
-mapping: Mapping = Mapping(BLOCK_LENGTH, ROBOT_SIZE)
+mapping_inst: Mapping = Mapping(BLOCK_LENGTH, ROBOT_SIZE)
 
 # for debugging
 loops = 0
@@ -54,19 +53,23 @@ prints = 10
 # - perform simulation steps until Webots is stopping the controller
 while robot.step(timestep) != -1:
     key = keyboard.getKey()
-    mapping.update(0, np.array([0, 0, 0]), lidar, lidar_axis=(0, 1))
+    mapping_inst.update(0, np.array([0, 0, 0]), lidar, lidar_axis=(0, 1))
     if key == ord("W"):
         pass
         #go forward
     #...
     elif key == ord("R"):
+        # something like this
+        """ path_planner.shortest_path(meters_to_blocks(localisation.get_coordinates(), BLOCK_LENGTH),
+                                   mapping_inst.origin,
+                                   mapping_inst.get()) """
         print(path_planner.test())
         pass
         # Ben
     # For debugging
     if (prints > 0) and (loops % prints == 0):
         # print(mapping.get())
-        print(mapping.get().shape)
+        print(mapping_inst.get().shape)
     loops += 1
 
     #localisation -> mapping -> databse of map
