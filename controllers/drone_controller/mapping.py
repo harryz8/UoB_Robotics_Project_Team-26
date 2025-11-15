fixed precision error

controllers/drone_controller/mapping.py
@@ -10,6 +10,11 @@ def _angle_calc_arr(robot_loc_meters: np.ndarray,
                     ) -> np.ndarray:
     # calculates angle from x-axis for vector between these two points when spec_loc_blocks is an np.ndarray and robot_loc is in meters not blocks
     lengths = spec_loc_blocks - (robot_loc_meters / block_length_meters)
+
+    # handles precision error on zeros
+    precision_error_mask = np.isclose(lengths, 0)
+    lengths[precision_error_mask] = 0
+
     return np.arctan2([lengths[:, axes[1]]], [lengths[:, axes[0]]])
 
 
@@ -41,10 +46,11 @@ def blocks_to_meters(block_indices_array: np.ndarray, block_length_meters: float
     return block_indices_array * block_length_meters
 
 
-# def displacement_2d(start: np.ndarray, end: np.ndarray, axis: tuple[int, int]) -> np.ndarray:
-#     print(f"end : {end}")
-#     print(f"start : {start}")
-#     return np.sqrt(np.square(end[:, axis[0]] - start[axis[0]]) + np.square(end[:, axis[1]] - start[axis[1]]))
+def visually_test_map(map_inst, mask: np.ndarray, z_index: int):
+    temp_map = np.zeros_like(map_inst.get())
+    for index in map_inst.get_all_map_indexes()[mask]:
+        temp_map[index[0], index[1], index[2]] = 1
+    print(temp_map[:, :, z_index])
 
 
 class Mapping:
@@ -126,7 +132,8 @@ class Mapping:
                                              (lidar_inst.axis_from_robot[1] + 1) % 3))
 
         # calculate filter
-        if (robot_attitude[2].item() - fov_components[0].item()) % (2*np.pi) > (robot_attitude[2].item() + fov_components[0].item()) % (2*np.pi):
+        if (robot_attitude[2].item() - fov_components[0].item()) % (2*np.pi) > (
+                robot_attitude[2].item() + fov_components[0].item()) % (2*np.pi):
             yaw_filter = np.logical_or(
                 (yaw_angles % (2*np.pi)) >= (
                         (robot_attitude[2].item() - fov_components[0].item()) % (2*np.pi)),

controllers/drone_controller/testing.py
@@ -33,10 +33,7 @@ def test_fov_mask(map_inst, lidar_inst):
     mask = map_inst.get_lidar_fov_mask(robot_loc=robot_loc,
                                        robot_attitude=np.array([0, 0, 0]),
                                        lidar_inst=lidar_inst)
-    temp_map = np.zeros_like(map_inst.get())
-    for index in map_inst.get_all_map_indexes()[mask]:
-        temp_map[index[0], index[1], index[2]] = 1
-    print(temp_map[:, :, 4])
+    visually_test_map(map_inst, mask, 4)
     assert map_inst.get_all_map_indexes()[mask].shape[0] == 45, "test_fov_mask() failed."
 
 def test_initialise_blocks_in_range(robot_map):
@@ -49,10 +46,7 @@ def test_range_mask(map_inst, lidar_inst):
     mask = map_inst.get_lidar_range_mask(robot_loc, lidar_inst)
     vals = blocks_to_meters(map_inst.get_all_map_indexes()[mask], map_inst.block_length) - robot_loc
     displacements = np.sqrt(np.sum(np.square(vals), axis=1))
-    temp_map = np.zeros_like(map_inst.get())
-    for index in map_inst.get_all_map_indexes()[mask]:
-        temp_map[index[0], index[1], index[2]] = 1
-    print(temp_map[:, :, 4])
+    visually_test_map(map_inst, mask, 4)
     assert (displacements <= 1).all(), "test_range_mask() failed."
 
 if __name__ == "__main__":
@@ -61,8 +55,8 @@ if __name__ == "__main__":
     range_image[range_image_filter] = np.inf
     mydar = Lidar(TestDevice(range_image), (0, 1), 0.9, 0.9)
     mymap = Mapping(250, 1, 10000)
-    test_initialise_blocks_in_range(mymap)
-    test_fov_mask(mymap, mydar)
-    test_range_mask(mymap, mydar)
-    # test_map_update(mymap, mydar)
+    # test_initialise_blocks_in_range(mymap)
+    # test_fov_mask(mymap, mydar)
+    # test_range_mask(mymap, mydar)
+    test_map_update(mymap, mydar)
     print("Everything passed")